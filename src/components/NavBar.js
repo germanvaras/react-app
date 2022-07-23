83b974b050bb327d30765c04e2cbfa542fbbe69c
German Varas <ger.varas.97@gmail.com>
2022-07-23 17:43:37 -0300

    component navBar creado

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import '../App.css';
+function NavBar(){
+    return(
+       <header className='header'>
+         <nav className='header-nav'>
+            <ul className='nav-ul'>
+                <a href="#" className='ul-li'><li>Home</li></a>
+                <a href="#" className='ul-li'><li>Blog</li></a>
+                <a href="#" className='ul-li'><li>Tienda</li></a>
+                <a href="#" className='ul-li'><li>Contacto</li></a>
+                <a href="#" className='ul-li'><li>Nosotros</li></a>
+            </ul>
+         </nav>
+       </header> 
+    )
+}
+export default NavBar
