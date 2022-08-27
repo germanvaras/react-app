3ddf2f3a632c81587e5d52d429d307c45447ea33
German Varas <ger.varas.97@gmail.com>
2022-08-27 09:07:34 -0300

    Footer creado

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from './components/NavBar/NavBar';
+import Footer from './components/Footer/Footer';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 import Cart from './components/Cart/Cart';
@@ -19,6 +20,7 @@ function App() {
             <Route path='cart' element={<Cart/>}/>
             <Route path='checkout' element={<Checkout/>}/> 
           </Routes>
+          <Footer/>
         </CartProvider>
       </BrowserRouter>
     </>
